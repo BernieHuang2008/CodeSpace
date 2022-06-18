19f0d4731afe232ab730ae265ff956849e985607
berniehuang2008 <berniehuang2008@163.com>
2022-06-18 16:26:17 +0800

    ud

--- LittleThings/AdminCLI/main.cpp
+++ LittleThings/AdminCLI/main.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
+#include"GAPI.h"
+#include<conio.h>
 using namespace std;
 int main()
 {
-    system("icon.txt");
+    string cmd;
+    char ch;
+    while(cmd!=""||cmd!="exit")
+    {
+        cout<<t::get()<<endl;
+        cout<<"          ________________________"<<endl;
+        cout<<"COMMAND: |________________________|\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+        ch=getch();
+        if(ch==13)
+        {
+            cout<<t::get()<<endl;
+        }
+        cmd+=ch;
+    }
 }
